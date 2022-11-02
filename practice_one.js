ac07c80f193b9f7cdf850222c724b3dade3400aa
Fatih Gur <fatih.gur26@hotmail.com>
2022-11-02 19:59:10 +0300

    First two part

---
+++ practice_one.js
@@ -0,0 +1,14 @@
+/* let firstName = "Fatih";
+let lastName = "Gur";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName); */
+
+/* let myName = "Fatih";
+let greeting = "Hi there";
+
+function greet() {
+    console.log(greeting + "," + myName + "!");
+}
+
+greet(); */
